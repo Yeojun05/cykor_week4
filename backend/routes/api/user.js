fd5213e587c4b2aa0002c31b0d3e6c1f552f7d88
Yeojun05 <danny200500@naver.com>
2025-06-06 00:03:07 +0900

    6_post-body에넣기

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();// navigater?
 
+const { check, validationResult } = require('express-validator'); 
 
-router.post('/',(req,res) =>{ 
+router.post('/',
+[
+    check('name','Name is required').not().isEmpty(),
+    check('password','more than 8').isLength({
+        min : 8
+    }),
+],
+async(req,res) =>{ 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors : errors.array()
+        })
+    }
     console.log(req.body);  //post
     res.send('user router');
 }
